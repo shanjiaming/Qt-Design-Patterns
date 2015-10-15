790c9cac3756ea8c53ca4d0a519d8d55cf262eb6
Xiao Che <xche03@gmail.com>
2015-10-15 12:16:18 -0600

    Example 2.3

---
+++ Ch2/Example2-3/Example2-3.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-10-15T12:10:18
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Example2-3
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES +=
+
+HEADERS += \
+    example2-3.h
---
+++ Ch2/Example2-3/example2-3.h
@@ -0,0 +1,16 @@
+#ifndef EXAMPLE23_H/*Header files are included in other files by the preprocessor. To prevent a header file
+from accidentally being included more than once in any compiled file, wrap it with*/
+#ifndef-#define ... #endif preprocessor macros
+#define EXAMPLE23_H
+#include <QString>
+class Fraction {
+public:
+void set(int numerator, int denominator);
+double toDouble() const;
+QString toString() const;
+private:
+int m_Numerator;
+int m_Denominator;
+};
+#endif // EXAMPLE23_H
+
